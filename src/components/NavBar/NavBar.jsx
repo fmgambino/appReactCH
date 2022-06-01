9b56240b22494f0767fec691efbec0e68b394ec7
fmgambino <fernando.m.gambino@gmail.com>
2022-06-01 18:18:56 -0300

    Updates Estilos NaBar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
+import '/src/components/NavBar/NavBar.css';
 import CartWidget from '../CartWidget/CartWidget'
 import LogoNavBar from '../logoNavBar/logoNavBar'
 
@@ -8,31 +9,34 @@ function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
   <LogoNavBar/>
-  <Navbar.Brand href="#home">CryptoPlus+</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+  {/* <Navbar.Brand href="#home">CryptoPlus+</Navbar.Brand> */}
+  <Navbar.Toggle id="botonNavBar" aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
     
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+      <Nav.Link href="#features">Home</Nav.Link>
+      <Nav.Link href="#pricing">About</Nav.Link>
+      <NavDropdown title="Servicios" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Desarrollo Hardware</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Desarrollo Software</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
+  
     <Nav>
       <Nav.Link href="#deets">More deets</Nav.Link>
       <Nav.Link eventKey={2} href="#memes">
         Dank memes
       </Nav.Link>
     </Nav>
+    
   </Navbar.Collapse>
- <CartWidget/>
- 
-  </Container>
+   <CartWidget/>
+   </Container>
+    
 </Navbar>
   )
 }
